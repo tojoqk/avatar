9f7f0bb884507a6be6f938a737747cc33cd0eb25
Masaya Tojo <masaya@tojo.tokyo>
2019-07-10 04:05:55 +0900

    Update!

--- tojoqk.rkt
+++ tojoqk.rkt
@@ -7,11 +7,11 @@
 (define bk 255)
 
 (define base (text "Tojo" 10 (make-color c0 c0 c0)))
-(define (p1 k) (text "Q" 6
+(define (p1 k) (text "Q" 10
                      (if (even? k)
                          (make-color c3 c3 c3)
                          (make-color c1 c1 c1))))
-(define (p2 k) (text "K" 6
+(define (p2 k) (text "K" 10
                      (if (even? k)
                          (make-color c1 c1 c1)
                          (make-color c3 c3 c3))))
@@ -23,7 +23,7 @@
               [(zero? n) base]
               [else
                (beside (p1 n)
-                       (scale 1.04 (rotate 110 (loop (- n 1))))
+                       (scale 1.06 (rotate 110 (loop (- n 1))))
                        (p2 n))]))]
          [background
           (circle (/ (image-width tojoqk) 2)
